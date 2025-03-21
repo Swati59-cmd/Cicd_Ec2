ce8907562704ef67534d80f4131a7ab31c916c6e
Swati59-cmd <swati.h@cloudshastrainfotech.com>
2025-03-21 15:20:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,109 @@
-resource "aws_instance" "example" {
-  ami           = "ami-08b5b3a93ed654d19"  # Change based on your region
-  instance_type = "t2.micro"
+#  Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
-    Name = var.ec2_name
+    Name = "MyTerraformVPC"
   }
 }
+
+# Create Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+# Create Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
+#  Create Internet Gateway (for Public Subnet)
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+#  Create Route Table for Public Subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+#  Associate Public Route Table with Public Subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+#  Create Security Group
+resource "aws_security_group" "web_sg" {
+  vpc_id      = aws_vpc.my_vpc.id
+  name        = "web_sg"
+  description = "Allow inbound SSH and HTTP traffic"
+
+  # Allow SSH (Port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP (Port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SG-TF"
+  }
+}
+resource "aws_instance" "my_ec2" {
+  ami                         = "ami-08b5b3a93ed654d19"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  security_groups             = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+  tags = {
+    Name = "EC2-TF"
+  }
+}
+
 
